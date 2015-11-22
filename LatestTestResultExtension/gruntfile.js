8bdc98a98e062ffdea8ce1c8a6e305dd0f4f6cb3
Kees Verhaar <keesv@delta-n.nl>
2015-11-22 15:18:23 +0100

    Initial commit

---
+++ LatestTestResultExtension/gruntfile.js
@@ -0,0 +1,47 @@
+﻿/// <binding BeforeBuild='exec:update, copy:main' AfterBuild='exec:package' ProjectOpened='exec:update, copy:main' />
+/*
+This file in the main entry point for defining grunt tasks and using grunt plugins.
+Click here to learn more. http://go.microsoft.com/fwlink/?LinkID=513275&clcid=0x409
+*/
+module.exports = function (grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
+        exec: {
+            package: {
+                command: "vset package -s settings.vset.json",
+                stdout: true,
+                stderr: true
+            },
+            update: {
+                command: "npm up --save-dev",
+                stdout: true,
+                stderr: true
+            },
+            publish: {
+                command: "vset publish -s settings.vset.json",
+                stdout: true,
+                stderr: true
+            }
+        },
+        copy: {
+            main: {
+                files: [
+                  // includes files within path
+                  { expand: true, flatten: true, src: ['node_modules/vss-sdk/lib/VSS.SDK.js'], dest: 'scripts/', filter: 'isFile' },
+                  //{ expand: true, flatten: true, src: ['node_modules/vss-sdk/typings/VSS.d.ts'], dest: 'scripts/ref', filter: 'isFile' },
+                   // { expand: true, flatten: true, src: ['node_modules/vss-sdk/typings/TFS.d.ts'], dest: 'scripts/ref', filter: 'isFile' }
+                ]
+            }
+        },
+        jasmine: {
+            src: ["scripts/**/*.js", "sdk/scripts/*.js"],
+            specs: "test/**/*[sS]pec.js",
+            helpers: "test/helpers/*.js"
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-exec");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-jasmine");
+
+};
---
+++ LatestTestResultExtension/scripts/testResultsTab.ts
@@ -0,0 +1,49 @@
+﻿/// <reference path='ref/VSS.d.ts' />
+/// <reference path='ref/q.d.ts' />
+
+import WorkItemServices = require("TFS/WorkItemTracking/Services");
+import TestManagementRestClient = require("TFS/TestManagement/RestClient");
+
+
+// Get the WorkItemFormService.  This service allows you to get/set fields/links on the 'active' work item (the work item
+// that currently is displayed in the UI).
+function getWorkItemFormService() {
+    return WorkItemServices.WorkItemFormService.getService();
+}
+
+var testClient = TestManagementRestClient.getClient();
+
+var testResultsPage = function () {
+    return {
+        // Called when a new work item is being loaded in the UI
+        onLoaded: function (args) {
+
+            getWorkItemFormService().then(function (service) {            
+                // Get the current values for a few of the common fields
+                service.getFieldValues(["System.Id", "System.Title", "System.State", "System.CreatedDate"]).then(
+                    function (value) {
+                        $(".events").append($("<div/>").text("onLoaded - " + JSON.stringify(value)));
+
+                        var id = +value["System.Id"];
+                        console.log("Work item id: " + id);
+
+                        var suites = TestManagementRestClient.getClient().getSuitesByTestCaseId(id).then(
+                            function (suites) {
+                                $.each(suites, (index, suite) => {
+                                    console.log("Suite: " + suite.name);
+                                });
+                            }
+                        );
+
+                    },
+                    function (error) {
+                        window.alert(error.message);
+                    });
+            });
+        }
+    }
+}
+
+VSS.register("test-results-page", testResultsPage);
+
+VSS.notifyLoadSucceeded();
